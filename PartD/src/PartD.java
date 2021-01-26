799b4fcc405090f76727fcd5a05638fb8d41dd2b
TysonTurnbull <tysonturnbull67@gmail.com>
2021-01-26 19:24:42 +1300

    Add favourite mode of transport

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Tyson Turnbull");
 		System.out.println("My favourite colour is dark green");
+		System.out.println("My favourite mode of transport is by aeroplane");
 	}
 
 }
