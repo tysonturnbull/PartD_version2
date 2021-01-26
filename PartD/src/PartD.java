6eb2817e095d77849c17938e05e51d89dc4c95a7
TysonTurnbull <tysonturnbull67@gmail.com>
2021-01-26 19:22:58 +1300

    Add favourite colour

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -3,6 +3,7 @@ public class PartD {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Tyson Turnbull");
+		System.out.println("My favourite colour is dark green");
 	}
 
 }
