f2ebe0255a09fb33424ccf62d3ea5b54e0cd215d
TysonTurnbull <tysonturnbull67@gmail.com>
2021-01-26 19:39:08 +1300

    add best day of the week

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		System.out.println("Hi, my name is Tyson Turnbull");
 		System.out.println("My favourite colour is dark green");
 		System.out.println("My favourite mode of transport is by aeroplane");
+		System.out.println("The best day of the week is Monday, but when that Monday is a holiday");
 	}
 
 }
